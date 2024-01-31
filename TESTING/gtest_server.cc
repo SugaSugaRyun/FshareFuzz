b207f3d0097a817b81eb47a3260bf2b1a847133a
SugaSugaRyun <rlarkfbs@gmail.com>
2024-01-31 16:18:02 +0900

    asdf

--- TESTING/gtest_server.cc
+++ TESTING/gtest_server.cc
@@ -1,8 +1,18 @@
 #include "gtest/gtest.h"
+#include "gtest/gmock.h"
 #include "server.h"
+#include <iostream>
+#include <string>
 
-TEST(ServerUnit__Test, ServerUnit__Test_1){
-    
+using namespace std;
+
+TEST(ServerUnit__TEST, ServerUnit__directory_check){
+    string s_path = "./googletest/CMakeFiles";
+    char * path = (char*)(s_path.c_str());
+    EXPECT_EQ(directory_check(path),EXIT_SUCCESS);
+    s_path = "./NOTaPATH";
+    path = (char*)(s_path.c_str()); 
+    EXPECT_EQ(directory_check(path),EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[]){
