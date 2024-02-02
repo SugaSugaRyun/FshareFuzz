e7a9660589546240bb6921c2d568dc719be2b753
leeejjju <leeejjju@gmail.com>
2024-02-02 17:51:46 +0900

    one case left

--- TESTING/gtest_client.cc
+++ TESTING/gtest_client.cc
@@ -5,24 +5,49 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
-#define BUF_SIZE 512
+#include <fstream>
 using namespace std;
 using namespace testing;
+using ::testing::Return;
+using ::testing::NiceMock;
+using ::testing::SetArgReferee;
+using ::testing::_;
+
+//wrapping origin c function mock
+class Vclass{
+public:
+    virtual ~Vclass() {}
+	virtual ssize_t send (int __fd, const void *__buf, size_t __n, int __flags){	return send(__fd, __buf, __n, __flags);}
+	virtual ssize_t recv (int __fd, void *__buf, size_t __n, int __flags){	return send(__fd, __buf, __n, __flags);}
+};
+
+//for mocking
+class Mclass : public Vclass  {
+public:
+	using Vclass::send;
+	using Vclass::recv;
+	MOCK_METHOD4(send, ssize_t(int __fd, const void *__buf, size_t __n, int __flags));
+	MOCK_METHOD4(recv, ssize_t(int __fd, void *__buf, size_t __n, int __flags));
+};
+
+
+NiceMock<Mclass>* mock;
+
 
 //override
-/*
 ssize_t send (int __fd, const void *__buf, size_t __n, int __flags){
-	return (ssize_t)__n;	
+	return mock->send(__fd, __buf, __n, __flags);
 }
 ssize_t recv (int __fd, void *__buf, size_t __n, int __flags){
-	return (ssize_t)__n;	
+	return mock->recv(__fd, __buf, __n, __flags);
 }
-*/
+
+
 
 TEST(ClientUnit__Test, get_cmd_code){
 
 	cmd rst;
-	char buf[BUF_SIZE];
+	char buf[buf_size];
 
 	strcpy(buf, "list");
 	rst = get_cmd_code(buf);
@@ -79,111 +104,264 @@ TEST(ClientUnit__Test, get_option){
 }
 
 TEST(ClientUnit__Test, parse_directory){
-	char toparse[BUF_SIZE];
+	char toparse[buf_size];
 	strcpy(toparse, "dir/dir/file");
 	char* parsed = parse_directory(toparse);
 	EXPECT_STREQ(parsed, "dir/dir");
 }
 
-/*
-//wrapping origin c function mock
-class Vclass{
-public:
-    virtual ~Vclass() {}
-	virtual ssize_t send (int __fd, const void *__buf, size_t __n, int __flags){	return send(__fd, __buf, __n, __flags);}
-	virtual ssize_t recv (int __fd, void *__buf, size_t __n, int __flags){	return send(__fd, __buf, __n, __flags);}
-};
-
-//for mocking
-class Mclass : public Vclass  {
-public:
-	using Vclass::send;
-	using Vclass::recv;
-	MOCK_METHOD4(send, ssize_t(int __fd, const void *__buf, size_t __n, int __flags));
-	MOCK_METHOD4(recv, ssize_t(int __fd, void *__buf, size_t __n, int __flags));
-};
-
-//TODO
 TEST(ClientUnit__Test, request){
+	mock = new NiceMock<Mclass>();
+	char target[buf_size];
+	FILE *fp;
 
-//Mclass mock;
+	//list
+	EXPECT_CALL(*mock, send)
+		.WillOnce(Return(sizeof(ch)));
 
-//	EXPECT_CALL(mock, send)
-//		.WillOnce(Return(100));
-	
+	fp = freopen("stdout_", "w", stdout);
+	ch.command = (cmd)0; //list
+	request(0);
+
+	freopen("/dev/tty", "w", stdout);
+    ifstream readFile("stdout_");
+    std::string strList((std::istreambuf_iterator<char>(readFile)), std::istreambuf_iterator<char>());
+	strcpy(target, ">> List request completed!\n");
+	EXPECT_STREQ(strList.c_str(), target); 
+
+	//get
+	EXPECT_CALL(*mock, send)
+		.WillOnce(Return(sizeof(ch)))
+		.WillOnce(Return(ch.payload_size));
+
+	fp = freopen("stdout_", "w", stdout);
+	ch.command = (cmd)1; //get
+	request(0);
+
+	freopen("/dev/tty", "w", stdout);
+    ifstream readFile2("stdout_");
+    std::string strGet((std::istreambuf_iterator<char>(readFile2)), std::istreambuf_iterator<char>());
+	strcpy(target, ">> Get request completed!\n");
+	EXPECT_STREQ(strGet.c_str(), target); 
+
+	//put
+	strcpy(file_path, "clientCloud/hello");
+	strcpy(dest_dir, "test");
+
+	EXPECT_CALL(*mock, send)
+		.WillOnce(Return(sizeof(ch)))
+		.WillOnce(Return(strlen(file_path) + strlen(dest_dir)))
+		.WillRepeatedly(Return(1));
+
+	fp = freopen("stdout_", "w", stdout);
+	ch.command = (cmd)2; //put
+	request(0);
+
+	freopen("/dev/tty", "w", stdout);
+    ifstream readFile3("stdout_");
+    std::string strPut((std::istreambuf_iterator<char>(readFile3)), std::istreambuf_iterator<char>());
+	strcpy(target, ">> Put request completed!\n");
+	EXPECT_STREQ(strPut.c_str(), target); 
 
 
+	delete(mock);
+	system("rm stdout_");
 }
-*/
 
-
-
-class aaA {
-public:
-    ~aaA() {}
-    virtual ssize_t recv(int __fd, void *__buf, size_t __n, int __flags){
-        return 0;
-    }
-};
-
-class MockB : public aaA{
-public:
-    MOCK_METHOD(ssize_t, recv, (int,void*,size_t,int));
-};
-
-
-MockB *mockB;  // B 함수의 mock 객체 생성
-ssize_t recv(int __fd, void *__buf, size_t __n, int __flags){
-	return mockB->recv(__fd, __buf, __n, __flags);
-}
-
-TEST(MyTest, TestAWithMockB) {
-    mockB = new MockB();
-    EXPECT_CALL(*mockB,recv).WillOnce(Return(sizeof(ch)));
-    int hihi = 0;
-    //  int result = mockB.B(1);
-	//recv(1, NULL, 1, 1);
-//    go_thread((void *)&hihi);
-    delete(mockB);
-}
-
-
-/*
 TEST(ClientUnit__Test, receive_list_response){
+	mock = new NiceMock<Mclass>();
+	FILE *fp, *tmp;
+
+	//receive error case
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(1));
+	tmp = stderr;
+	fp = fopen("stderr_", "w");
+	stderr = fp;
+	receive_list_response(0);
+
+	stderr = tmp;
+	fclose(fp);
+
+    ifstream readFile("stderr_");
+    std::string strErr((std::istreambuf_iterator<char>(readFile)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strErr.find("receive error :") != string::npos);
 
 
+	//response error case
+	sh.is_error = 1;
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(sizeof(sh)));
+	tmp = stderr;
+	fp = fopen("stderr_", "w");
+	stderr = fp;
+	receive_list_response(0);
+
+	stderr = tmp;
+	fclose(fp);
+
+    ifstream readFile2("stderr_");
+    std::string strErr2((std::istreambuf_iterator<char>(readFile2)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strErr2.find("response error :") != string::npos);
+	sh.is_error = 0;	
+
+
+	//normal case - recv once
+	sh.payload_size = 10;
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(sizeof(sh)))
+		.WillOnce(Return(sh.payload_size))
+		.WillOnce(Return(0));
+
+	fp = freopen("stdout_", "w", stdout);
+	receive_list_response(0);
+
+	freopen("/dev/tty", "w", stdout);
+    ifstream readFile3("stdout_");
+    std::string strOut((std::istreambuf_iterator<char>(readFile3)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strOut.find(">") != string::npos);
+
+	
+	//receive error 2 case
+	sh.payload_size = 10;
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(sizeof(sh)))
+		.WillOnce(Return(1));
+
+	tmp = stderr;
+	fp = fopen("stderr_", "w");
+	stderr = fp;
+	receive_list_response(0);
+
+	stderr = tmp;
+	fclose(fp);
+
+    ifstream readFile4("stderr_");
+    std::string strErr3((std::istreambuf_iterator<char>(readFile4)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strErr3.find("receive error 2 :") != string::npos);
+
+
+	//normal case - recv multiple time
+	sh.payload_size = buf_size + 10;
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(sizeof(sh)))
+		.WillOnce(Return(buf_size))
+		.WillOnce(Return(10))
+		.WillOnce(Return(0));
+
+	fp = freopen("stdout_", "w", stdout);
+	receive_list_response(0);
+
+	freopen("/dev/tty", "w", stdout);
+    ifstream readFile5("stdout_");
+    std::string strOut2((std::istreambuf_iterator<char>(readFile5)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strOut2.find(">") != string::npos);
+
+	delete(mock);
+	system("rm stdout_");
+	system("rm stderr_");
 }
 
 
 TEST(ClientUnit__Test, make_directory){
 
-	EXPECT_EQ(0, 1);
+	char towrite[buf_size];
+	FILE* fp; 
+	//already exist case
+	fp = freopen("stdout_", "w", stdout);
+	strcpy(towrite, "dir_exist/test");
+	struct stat info;
+    if(stat(towrite, &info) == 0 && S_ISDIR(info.st_mode)) system("mkdir dir_exist");
+	make_directory(towrite);
+
+	freopen("/dev/tty", "w", stdout);
+	ifstream readFile("stdout_");
+    std::string strOut((std::istreambuf_iterator<char>(readFile)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strOut.find("dir :") != string::npos);
+
+	//mkdir case
+	fp = freopen("stdout_", "w", stdout);
+	system("rm -rf dir_not_exist");
+	strcpy(towrite, "dir_not_exist/test");
+	make_directory(towrite);
+
+	freopen("/dev/tty", "w", stdout);
+	ifstream readFile2("stdout_");
+    std::string strOut2((std::istreambuf_iterator<char>(readFile2)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strOut2.find("dir :") != string::npos);
+
+	system("rm stdout_");
 
 }
 
 
 
 TEST(ClientUnit__Test, receive_get_response){
-
+	printf("TODO\n");
 	EXPECT_EQ(0, 1);
 
 }
-
 
 
 TEST(ClientUnit__Test, receive_put_response){
+	mock = new NiceMock<Mclass>();
+	FILE *fp, *tmp;
+	char target[buf_size];
 
-	EXPECT_EQ(0, 1);
+	//normal case
+	sh.is_error = 0;
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(sizeof(sh)));
+	sprintf(target, ">> Writing %s on server succesfully completed!\n", basename(file_path));
+	fp = freopen("stdout_", "w", stdout);
+	receive_put_response(0);
+	
+	freopen("/dev/tty", "w", stdout);
+    ifstream readFile("stdout_");
+    std::string strList((std::istreambuf_iterator<char>(readFile)), std::istreambuf_iterator<char>());
+	EXPECT_STREQ(strList.c_str(), target); 
+
+
+	//receive error case
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(0));
+	tmp = stderr;
+	fp = fopen("stderr_", "w");
+	stderr = fp;
+	receive_put_response(0);
+
+	stderr = tmp;
+	fclose(fp);
+
+    ifstream readFile2("stderr_");
+    std::string strErr1((std::istreambuf_iterator<char>(readFile2)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strErr1.find("receive error :") != string::npos);
+	
+
+	//response error case
+	EXPECT_CALL(*mock, recv)
+		.WillOnce(Return(sizeof(sh)));
+	sh.is_error = 1;
+	tmp = stderr;
+	fp = fopen("stderr_", "w");
+	stderr = fp;
+	receive_put_response(0);
+
+	stderr = tmp;
+	fclose(fp);
+
+    ifstream readFile3("stderr_");
+    std::string strErr2((std::istreambuf_iterator<char>(readFile3)), std::istreambuf_iterator<char>());
+    EXPECT_TRUE(strErr2.find("response error :") != string::npos);
+
+	delete(mock);
+	system("rm stdout_");
+	system("rm stderr_");
 
 }
 
 
-TEST(ClientUnit__Test, receive_response){
-
-	EXPECT_EQ(0, 1);
-
-}
-*/
+//TEST(ClientUnit__Test, receive_response){}
 
 
 
